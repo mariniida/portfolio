81cc376636ca0b4c219ee6966091e1f57ddc72e5
Marin <mariniida114@gmail.com>
2020-03-14 15:45:36 +0900

    assetsPublicPath

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -14,7 +14,7 @@
               :href="link.url">
               <el-button class="button">
                 <i :class="link.icon"></i>
-              	{{link.name}}
+              	{{link.name}}yay
               </el-button>
               </a>
             </li>
